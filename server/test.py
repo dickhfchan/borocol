f4d82843fe38347893c4065b523e062db5eb1d85
phphe <phphe@outlook.com>
2017-12-02 11:12:09 +0800

    test servert server

--- server/test.py
+++ server/test.py
@@ -1,21 +1,8 @@
 from config import keyspace, host, debug, api_port
-from flask import Flask, jsonify, request, Response
+from cassandra.cqlengine import connection
+import models
+import json
 
-app = Flask(__name__)
+connection.setup([host], keyspace)
 
-#print "Make connection to DB"
-#print conn
-
-default_headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods':'GET,POST,PUT,PATCH,DELETE,OPTIONS',
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Connection, User-Agent, Cookie'
-    }
-
-@app.route('/')
-def index():
-    print int(request.args.get('page')or 1)
-    return "Hello_World"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=api_port, debug=debug, threaded=True)
+rows = models.User.objects().all()[0:20]
