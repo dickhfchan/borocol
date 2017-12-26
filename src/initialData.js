7e6dcde86ebec3acbc0951a8ec66aacd5bbc52f1
phphe <phphe@outlook.com>
2017-12-26 15:34:11 +0800

    build

--- src/initialData.js
+++ src/initialData.js
@@ -1,5 +1,10 @@
-const initialData = process.env.NODE_ENV === 'production' ? window.initialData : {
+const initialData = window.initialData ||  {
   serverRoot: '',
   clientBase: '/',
+}
+if (process.env.NODE_ENV === 'production') {
+  Object.assign(initialData, {
+    clientBase: '/borocol/dist/',
+  })
 }
 export default initialData
