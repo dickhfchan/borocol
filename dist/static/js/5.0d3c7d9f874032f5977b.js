a0eaf45e0d24473b728f7dcbde9e39075e458e74
phphe <phphe@outlook.com>
2018-01-02 16:36:48 +0800

    build

---
+++ dist/static/js/5.0d3c7d9f874032f5977b.js
@@ -0,0 +1,2 @@
+webpackJsonp([5],{FUGo:function(e,t){},j7e0:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r={render:function(){var e=this.$createElement,t=this._self._c||e;return t("div",{staticClass:"Home container"},[t("div",{staticClass:"content-card"},[t("div",[t("router-link",{attrs:{to:{name:"createCourse"}}},[this._v("Create Course ")])],1)])])},staticRenderFns:[]},s=n("OF7X")({components:{},data:function(){return{}}},r,!1,function(e){n("FUGo")},null,null);t.default=s.exports}});
+//# sourceMappingURL=5.0d3c7d9f874032f5977b.js.map
