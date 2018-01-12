e980ce8f7bb18a493ebc6d473e8dc980bbf7f32d
phphe <phphe@outlook.com>
2018-01-12 14:13:01 +0800

    change build output path

--- config/index.js
+++ config/index.js
@@ -43,12 +43,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../server/static/index.html'),
+    index: path.resolve(__dirname, '../server/templates/user-admin.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../server/static'), // output to production directory; 直接输出到工作目录
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/static/',
+    assetsRoot: path.resolve(__dirname, '../server/static/user-admin'), // output to production directory; 直接输出到工作目录
+    assetsSubDirectory: '',
+    assetsPublicPath: '/static/user-admin/',
 
     /**
      * Source Maps
