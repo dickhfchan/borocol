5f4cea3f9e4dc3674faa3c3a8d2dc9c99d8e9497
phphe <phphe@outlook.com>
2018-01-24 19:54:56 +0800

    MyCourseVisaStep1PopupVisa done

---
+++ src/views/MyCourseVisaStep1PopupVisa.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+include ../common.pug
+.MyCourseVisa
+  .container
+    .content-card.has-close
+      .title Upload Files
+      .content-card-body
+        MultipleImageUploader.mtl.w-100(v-model="photos" :visibleBlockCount="4" :boxWidth="220" :boxHeight="310")
+        .text-center
+          button.submit-btn.btn.btn-primary.btn-lg Submit
+      span.icon.icon-close.close
+</template>
+
+<script>
+import MultipleImageUploader from '@/components/MultipleImageUploader';
+export default {
+  components: {MultipleImageUploader},
+  data() {
+    return {
+      photos: [],
+    }
+  },
+  // computed: {},
+  // watch: {},
+  // methods: {},
+  // created() {},
+  // mounted() {},
+}
+</script>
+
+<style lang="scss">
+@import "~@/assets/css/global.scss";
+.MyCourseVisa{
+  background: $bg3;
+  // common
+  .submit-btn{
+    width: 420px;
+    margin-top: 100px;
+  }
+  .title {
+    font-size: 21px;
+    color: rgb(51, 51, 51);
+    text-align: center;
+  }
+}
+</style>
