f0aa66a8e8320c31098cec1313cecd1c105e12f1
phphe <phphe@outlook.com>
2018-01-24 20:24:30 +0800

    fix tooltip position

--- src/components/Tooltip.vue
+++ src/components/Tooltip.vue
@@ -1,22 +1,43 @@
 <template lang="pug">
 .Tooltip
   slot
-  .tooltip.bottom(role='tooltip')
+  .tooltip.bottom(role='tooltip' ref="tip" :style="style")
     .tooltip-arrow
     .tooltip-inner {{title}}
 </template>
 
 <script>
+import mounted from './mounted'
 export default {
+  mixins: [mounted],
   props: {
     title: {},
   },
   // components: {},
-  // data() {
-  //   return {}
-  // },
+  data() {
+    return {
+      style: {
+        left: null,
+        display: '',
+      }
+    }
+  },
   // computed: {},
-  // watch: {},
+  watch: {
+    title: {
+      immediate: true,
+      handler(value) {
+        this.mounted.then(() => {
+          this.style.display = 'block'
+          this.$nextTick(() => {
+            const tipWidth = this.$refs.tip.offsetWidth
+            this.style.left = (this.$el.offsetWidth - tipWidth) / 2 + 'px'
+            this.style.display = ''
+          })
+        })
+      }
+    }
+  },
   // methods: {},
   // created() {},
   // mounted() {},
@@ -29,7 +50,6 @@ export default {
   position: relative;
   .tooltip{
     opacity: 1;
-    left: -50%;
     display: none;
   }
   &:hover{
