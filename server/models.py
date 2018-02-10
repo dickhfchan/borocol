265a4682de6b063509f9342eeb685d856c32fdb1
phphe <phphe@outlook.com>
2018-02-10 11:05:46 +0800

    index all columns

--- server/models.py
+++ server/models.py
@@ -8,243 +8,245 @@ class user(Model, UserMixin):
 
     id      = columns.UUID(required=True, primary_key=True)
 
-    password      = columns.Bytes(required=False, )
+    password      = columns.Bytes(required=False, index=True, )
 
-    privacy      = columns.Text(required=False, )
+    privacy      = columns.Text(required=False, index=True, )
 
     email      = columns.Text(required=False, index=True, )
 
-    user_type      = columns.Text(required=False, )
+    user_type      = columns.Text(required=False, index=True, )
 
-    created_at = columns.DateTime()
-    updated_at = columns.DateTime()
+    created_at = columns.DateTime(index=True, )
+    updated_at = columns.DateTime(index=True, )
 
 class course_subscriptions(Model):
 
     id      = columns.UUID(required=True, primary_key=True)
 
-    user_id      = columns.Text(required=False, )
+    user_id      = columns.Text(required=False, index=True, )
 
-    course_id      = columns.Text(required=False, )
+    course_id      = columns.Text(required=False, index=True, )
 
-    require_accomodation      = columns.Text(required=False, )
+    require_accomodation      = columns.Text(required=False, index=True, )
 
-    require_insurance      = columns.Text(required=False, )
+    require_insurance      = columns.Text(required=False, index=True, )
 
-    payment_details      = columns.Text(required=False, )
+    payment_details      = columns.Text(required=False, index=True, )
 
-    created_at = columns.DateTime()
-    updated_at = columns.DateTime()
+    created_at = columns.DateTime(index=True, )
+    updated_at = columns.DateTime(index=True, )
 
 class school_profile(Model):
 
     id      = columns.UUID(required=True, primary_key=True)
 
-    user_id      = columns.Text(required=False, )
+    user_id      = columns.Text(required=False, index=True, )
 
-    status      = columns.Text(required=False, )
+    status      = columns.Text(required=False, index=True, )
 
-    contact_persons      = columns.Text(required=False, )
+    contact_persons      = columns.Text(required=False, index=True, )
 
-    school_name      = columns.Text(required=False, )
+    school_name      = columns.Text(required=False, index=True, )
 
-    registration_document      = columns.Text(required=False, )
+    registration_document      = columns.Text(required=False, index=True, )
 
-    stripe_details      = columns.Text(required=False, )
+    stripe_details      = columns.Text(required=False, index=True, )
 
-    address1      = columns.Text(required=False, )
+    address1      = columns.Text(required=False, index=True, )
 
-    address2      = columns.Text(required=False, )
+    address2      = columns.Text(required=False, index=True, )
 
-    city      = columns.Text(required=False, )
+    city      = columns.Text(required=False, index=True, )
 
-    country      = columns.Text(required=False, )
+    country      = columns.Text(required=False, index=True, )
 
-    introduction      = columns.Text(required=False, )
+    introduction      = columns.Text(required=False, index=True, )
 
-    logo      = columns.Text(required=False, )
+    logo      = columns.Text(required=False, index=True, )
 
-    school_photos      = columns.Text(required=False, )
+    school_photos      = columns.Text(required=False, index=True, )
 
-    subscriptions      = columns.Text(required=False, )
+    subscriptions      = columns.Text(required=False, index=True, )
 
-    website      = columns.Text(required=False, )
+    website      = columns.Text(required=False, index=True, )
 
-    created_at = columns.DateTime()
-    updated_at = columns.DateTime()
+    created_at = columns.DateTime(index=True, )
+    updated_at = columns.DateTime(index=True, )
 
 class student_profile(Model):
 
     id      = columns.UUID(required=True, primary_key=True)
-    user_id      = columns.Text(required=False, )
-    status      = columns.Text(required=False, )
-    currency      = columns.Text(required=False, )
-    language      = columns.Text(required=False, )
-    liked_courses      = columns.Text(required=False, )
+    user_id      = columns.Text(required=False, index=True, )
+    status      = columns.Text(required=False, index=True, )
+    currency      = columns.Text(required=False, index=True, )
+    language      = columns.Text(required=False, index=True, )
+    liked_courses      = columns.Text(required=False, index=True, )
 
     # in student profile form
-    avatar = columns.Text(required=False, )
-    first_name = columns.Text(required=False, )
-    middle_name = columns.Text(required=False, )
-    last_name = columns.Text(required=False, )
-    gender = columns.Text(required=False, )
-    birthday = columns.DateTime(required=False, )
-    nationality = columns.Text(required=False, )
-    country_of_residence = columns.Text(required=False, )
-    email = columns.Text(required=False, )
-    phone = columns.Text(required=False, )
-    passport_info = columns.Text(required=False, )
-    emergency_contact_person = columns.Text(required=False, )
+    avatar = columns.Text(required=False, index=True, )
+    first_name = columns.Text(required=False, index=True, )
+    middle_name = columns.Text(required=False, index=True, )
+    last_name = columns.Text(required=False, index=True, )
+    gender = columns.Text(required=False, index=True, )
+    birthday = columns.DateTime(required=False, index=True, )
+    nationality = columns.Text(required=False, index=True, )
+    country_of_residence = columns.Text(required=False, index=True, )
+    email = columns.Text(required=False, index=True, )
+    phone = columns.Text(required=False, index=True, )
+    passport_info = columns.Text(required=False, index=True, )
+    emergency_contact_person = columns.Text(required=False, index=True, )
 
-    created_at = columns.DateTime()
-    updated_at = columns.DateTime()
+    created_at = columns.DateTime(index=True, )
+    updated_at = columns.DateTime(index=True, )
 
 class course_detail(Model):
+    fileFields = ['instructor_photo', 'cover', 'photos']
 
     id      = columns.UUID(required=True, primary_key=True)
-    school_id      = columns.Text(required=False, )
+    school_id      = columns.Text(required=False, index=True, )
 
-    name = columns.Text(required=True, )
-    category_id = columns.Text(required=True, )
-    level = columns.Text(required=True, )
-    start_date = columns.DateTime(required=True, )
-    end_date = columns.DateTime(required=True, )
-    description = columns.Text(required=False, )
-    group_size = columns.Integer(required=True, )
-    gender = columns.Text(required=False, )
-    age_range = columns.List(columns.Integer, required=False, )
-    hours = columns.Integer(required=True, )
-    language = columns.Text(required=True, )
-    instructor_photo = columns.Text(required=False, )
-    instructor_info = columns.Text(required=False, )
-    issue_certificate = columns.Boolean(required=True, )
-    certificate = columns.Text(required=False, )
-    address = columns.Text(required=True, )
-    city = columns.Text(required=True, )
-    country = columns.Text(required=True, )
-    api_key = columns.Text(required=True, )
-    location_description = columns.Text(required=False, )
-    how_to_get_there = columns.Text(required=False, )
-    where_to_meet = columns.Text(required=False, )
-    schedule = columns.Text(required=True, )
-    meals_included = columns.Boolean(required=True, )
-    meals = columns.List(columns.Text, required=False, )
-    meals_info = columns.Text(required=False, )
-    provide = columns.Text(required=False, )
-    guest_needs_to_bring = columns.Text(required=False, )
-    guest_requirement = columns.Text(required=False, )
-    request_form_existed = columns.Boolean(required=True, )
-    request_form = columns.Text(required=False, )
-    tags = columns.List(columns.Text, required=False, )
-    cover = columns.Text(required=True, )
-    photos = columns.List(columns.Text, required=True, )
-    notes = columns.Text(required=False, )
-    seats = columns.Integer(required=True, )
-    price = columns.Decimal(required=True, )
-    registration_start_date = columns.DateTime(required=True, )
-    registration_end_date = columns.DateTime(required=True, )
-    early_bird_discount = columns.Boolean(required=False, )
-    discount_rate = columns.Text(required=False, )
-    quota = columns.Text(required=False, )
-    down_payment = columns.Boolean(required=False, )
+    name = columns.Text(required=True, index=True, )
+    category_id = columns.Text(required=True, index=True, )
+    level = columns.Text(required=True, index=True, )
+    start_date = columns.DateTime(required=True, index=True, )
+    end_date = columns.DateTime(required=True, index=True, )
+    description = columns.Text(required=False, index=True, )
+    group_size = columns.Integer(required=True, index=True, )
+    gender = columns.Text(required=False, index=True, )
+    age_range = columns.List(columns.Integer, required=False, index=True, )
+    hours = columns.Integer(required=True, index=True, )
+    language = columns.Text(required=True, index=True, )
+    instructor_photo = columns.Text(required=False, index=True, )
+    instructor_info = columns.Text(required=False, index=True, )
+    issue_certificate = columns.Boolean(required=True, index=True, )
+    certificate = columns.Text(required=False, index=True, )
+    address = columns.Text(required=True, index=True, )
+    city = columns.Text(required=True, index=True, )
+    country = columns.Text(required=True, index=True, )
+    api_key = columns.Text(required=True, index=True, )
+    location_description = columns.Text(required=False, index=True, )
+    how_to_get_there = columns.Text(required=False, index=True, )
+    where_to_meet = columns.Text(required=False, index=True, )
+    schedule = columns.Text(required=True, index=True, )
+    meals_included = columns.Boolean(required=True, index=True, )
+    meals = columns.List(columns.Text, required=False, index=True, )
+    meals_info = columns.Text(required=False, index=True, )
+    provide = columns.Text(required=False, index=True, )
+    guest_needs_to_bring = columns.Text(required=False, index=True, )
+    guest_requirement = columns.Text(required=False, index=True, )
+    request_form_existed = columns.Boolean(required=True, index=True, )
+    request_form = columns.Text(required=False, index=True, )
+    tags = columns.List(columns.Text, required=False, index=True, )
+    cover = columns.Text(required=True, index=True, )
+    photos = columns.List(columns.Text, required=True, index=True, )
+    notes = columns.Text(required=False, index=True, )
+    seats = columns.Integer(required=True, index=True, )
+    price = columns.Decimal(required=True, index=True, )
+    registration_start_date = columns.DateTime(required=True, index=True, )
+    registration_end_date = columns.DateTime(required=True, index=True, )
+    early_bird_discount = columns.Boolean(required=False, index=True, )
+    discount_rate = columns.Text(required=False, index=True, )
+    quota = columns.Text(required=False, index=True, )
+    down_payment = columns.Boolean(required=False, index=True, )
 
-    created_at = columns.DateTime()
-    updated_at = columns.DateTime()
+    created_at = columns.DateTime(index=True, )
+    updated_at = columns.DateTime(index=True, )
 
 class accomodation_detail(Model):
+    fileFields = ['photos']
 
     id      = columns.UUID(required=True, primary_key=True)
-    course_id      = columns.Text(required=False, )
+    course_id      = columns.Text(required=False, index=True, )
 
-    options = columns.List(columns.Text, required=False, )
-    other_options = columns.Text(required=False, )
-    location_description = columns.Text(required=False, )
-    facilities = columns.List(columns.Text, required=False, )
-    other_facilities = columns.Text(required=False, )
-    photos = columns.List(columns.Text, required=False, )
-    room1_enabled = columns.Boolean(required=False, )
-    room1_type = columns.Text(required=False, )
-    room1_quota = columns.Text(required=False, )
-    room1_price = columns.Text(required=False, )
-    room2_enabled = columns.Boolean(required=False, )
-    room2_type = columns.Text(required=False, )
-    room2_quota = columns.Text(required=False, )
-    room2_price = columns.Text(required=False, )
+    options = columns.List(columns.Text, required=False, index=True, )
+    other_options = columns.Text(required=False, index=True, )
+    location_description = columns.Text(required=False, index=True, )
+    facilities = columns.List(columns.Text, required=False, index=True, )
+    other_facilities = columns.Text(required=False, index=True, )
+    photos = columns.List(columns.Text, required=False, index=True, )
+    room1_enabled = columns.Boolean(required=False, index=True, )
+    room1_type = columns.Text(required=False, index=True, )
+    room1_quota = columns.Text(required=False, index=True, )
+    room1_price = columns.Text(required=False, index=True, )
+    room2_enabled = columns.Boolean(required=False, index=True, )
+    room2_type = columns.Text(required=False, index=True, )
+    room2_quota = columns.Text(required=False, index=True, )
+    room2_price = columns.Text(required=False, index=True, )
 
-    created_at = columns.DateTime()
-    updated_at = columns.DateTime()
+    created_at = columns.DateTime(index=True, )
+    updated_at = columns.DateTime(index=True, )
 
 class visa_detail(Model):
 
     id      = columns.UUID(required=True, primary_key=True)
 
-    course_id      = columns.Text(required=False, )
+    course_id      = columns.Text(required=False, index=True, )
 
-    information      = columns.Text(required=False, )
+    information      = columns.Text(required=False, index=True, )
 
-    last_date_submission      = columns.Text(required=False, )
+    last_date_submission      = columns.Text(required=False, index=True, )
 
-    document_description      = columns.Text(required=False, )
+    document_description      = columns.Text(required=False, index=True, )
 
-    visa_url      = columns.Text(required=False, )
+    visa_url      = columns.Text(required=False, index=True, )
 
-    schooli_invitation_letter      = columns.Text(required=False, )
+    schooli_invitation_letter      = columns.Text(required=False, index=True, )
 
-    refund_policy      = columns.Text(required=False, )
+    refund_policy      = columns.Text(required=False, index=True, )
 
-    auto_reply_message      = columns.Text(required=False, )
+    auto_reply_message      = columns.Text(required=False, index=True, )
 
-    created_at = columns.DateTime()
-    updated_at = columns.DateTime()
+    created_at = columns.DateTime(index=True, )
+    updated_at = columns.DateTime(index=True, )
 
 class featured_course(Model):
 
     id      = columns.UUID(required=True, primary_key=True)
 
-    course_id      = columns.Text(required=False, )
+    course_id      = columns.Text(required=False, index=True, )
 
-    created_at = columns.DateTime()
-    updated_at = columns.DateTime()
+    created_at = columns.DateTime(index=True, )
+    updated_at = columns.DateTime(index=True, )
 
 class reviews(Model):
 
     id      = columns.UUID(required=True, primary_key=True)
 
-    user_id      = columns.Text(required=False, )
+    user_id      = columns.Text(required=False, index=True, )
 
-    course_id      = columns.Text(required=False, )
+    course_id      = columns.Text(required=False, index=True, )
 
-    ratings      = columns.Text(required=False, )
+    ratings      = columns.Text(required=False, index=True, )
 
-    comments      = columns.Text(required=False, )
+    comments      = columns.Text(required=False, index=True, )
 
-    photos      = columns.Text(required=False, )
+    photos      = columns.Text(required=False, index=True, )
 
-    created_at = columns.DateTime()
-    updated_at = columns.DateTime()
+    created_at = columns.DateTime(index=True, )
+    updated_at = columns.DateTime(index=True, )
 
 class course_types(Model):
 
     id      = columns.UUID(required=True, primary_key=True)
 
-    name      = columns.Text(required=False, )
+    name      = columns.Text(required=False, index=True, )
 
-    type      = columns.Text(required=False, )
+    type      = columns.Text(required=False, index=True, )
 
-    description      = columns.Text(required=False, )
+    description      = columns.Text(required=False, index=True, )
 
-    created_at = columns.DateTime()
-    updated_at = columns.DateTime()
+    created_at = columns.DateTime(index=True, )
+    updated_at = columns.DateTime(index=True, )
 
 class test(Model):
 
     id      = columns.UUID(required=True, primary_key=True)
-    ls      = columns.List(columns.Text, required=False, )
-    lsi      = columns.List(columns.Integer, required=False, )
-    dm      = columns.Decimal(required=False, )
+    ls      = columns.List(columns.Text, required=False, index=True, )
+    lsi      = columns.List(columns.Integer, required=False, index=True, )
+    dm      = columns.Decimal(required=False, index=True, )
 
-    created_at = columns.DateTime()
-    updated_at = columns.DateTime()
+    created_at = columns.DateTime(index=True, )
+    updated_at = columns.DateTime(index=True, )
 
 # this func will auto create tables
 # coonect database before do this
