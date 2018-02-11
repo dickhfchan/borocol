e3ae88f0ab0f185ef29643b55c2eba8a305a3640
phphe <phphe@outlook.com>
2018-02-11 21:56:11 +0800

    fix

--- server/controllers/IndexController.py
+++ server/controllers/IndexController.py
@@ -1,18 +1,19 @@
 from flask import current_app as app, request, render_template
 import json
-from controllers import UserController
+from UserController import UserController
 from flask_login import current_user
 
 class IndexController(object):
     def _renderSpa(self, fp):
         html = render_template(fp)
         initialData = {'serverRoot': '', 'clientBase': '/'} # serverRoot cant end with /
-        html = html.replace('<head>', '<head><script>var initialData = %s;</script>'%(json.dumps(initialData)))
         # inject user info
         if current_user.is_authenticated:
             c = UserController()
             initialData['authenticated'] = True
-            initialData['user'] = c.get_user_dict()
+            initialData['user'] = c.get_user_dict(current_user)
+        # 
+        html = html.replace('<head>', '<head><script>var initialData = %s;</script>'%(json.dumps(initialData)))
         return html
     def index(self):
         return self._renderSpa('index.html')
