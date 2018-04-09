b0114ef3a8fea3cd5c1186c166d92d8de8fe59cc
phphe <phphe@outlook.com>
2018-04-09 20:09:13 +0800

    deploy

--- server/gunicorn-config.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append('./')
-import config as appConfig
-
-bind = '127.0.0.1:8000'      #绑定ip和端口号
-workers = 4
-errorlog = './gunicorn.log'
-loglevel = 'debug' if appConfig.app_debug else 'error'
