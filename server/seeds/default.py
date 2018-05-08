f9c66f8da003d2811401dbb56b490c31661af97f
phphe <phphe@outlook.com>
2018-05-08 09:47:45 +0800

    seed

--- server/seeds/default.py
+++ server/seeds/default.py
@@ -2,6 +2,7 @@ from plugins.seeder import fake, pop
 import utils as ut
 import models
 from plugins.ResourceController import store, update
+import random
 
 # student
 emails = ['1@test.com', '2@test.com']
@@ -57,3 +58,68 @@ for i in range(5):
         'photos' : fake.imgs(),
     }
     profile = store(models.school_profile, data)
+# course
+schools = []
+for user in models.user.all():
+    if user.user_type == 'school':
+        schools.append(user)
+        if len(schools) == 20:
+            break
+for i in range(5):
+    school = fake.rand(*schools)
+    with_accom = random.random() > 0.3
+    data = {
+        'school_id': school.id,
+        # 
+        'with_accom': with_accom,
+        #
+        'start_date': fake.ts(),
+        'end_date': fake.ts(),
+        'category_id': fake.word(),
+        'level': fake.word(),
+        'title': fake.sentence(),
+        #
+        'gender': 'male',
+        'age_range': [16,50],
+        'hours': [20,50],
+        'description': fake.sentence(),
+        #
+        'language': fake.word(),
+        'instructors': fake.word(),todo
+        #
+        'street': fake.sentence(),
+        'city': fake.word(),
+        'province': fake.word(),
+        'zip_code': fake.word(),
+        'country': fake.country_code(),
+        'api_key': fake.word(),
+        'location_description': fake.sentences(),
+        'how_to_get_there': fake.sentences(),
+        'where_to_meet': fake.sentences(),
+        #
+        'schedule': fake.sentences(),
+        'meals_included': todo,
+        'meals': todo,
+        'weather_arrangement': fake.sentences(),
+        #
+        'provide': fake.sentences(),
+        'guest_needs_to_bring': fake.sentences(),
+        'issue_certificate': todo,
+        'certificate': fake.sentences(),
+        #
+        'entry_requirment': fake.sentences(),
+        'request_form_enabled': True,
+        'request_form': fake.word(),todo
+        #
+        'group_size': fake.random_int(),
+        'seat_quota': fake.random_int(),
+        'price': 66.66,
+        #
+        'early_bird': fake.word(),todo
+        'down_payment': fake.word(),todo
+        #
+        'cover': fake.img(),
+        'photos': fake.imgs(),
+        'youtube_video_link': fake.img(),
+        'tags': ['Design', 'IT'],
+    }
